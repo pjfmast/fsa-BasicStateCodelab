0ca2bd03aa4c7e2c7b6735e4e334d19e756a2293
Paul de Mast <pjf.demast@avans.nl>
2024-11-23 14:09:08 +0100

    added WellnessViewModel.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "avans.avd.basicstatecodelab"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
